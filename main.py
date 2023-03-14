335c92949ded55a56204bde7ecb3a93c8820cc63
GrishenkoKristina <grisenkokristina2@gmail.com>
2023-03-14 20:05:30 +0300

    uidyqy

---
+++ main.py
@@ -0,0 +1,48 @@
+def lab31():
+words=[]
+# прогарамма останавливается при вводе слова "stop"
+while (new_word:= str(input())) != "stop":
+    words.append(new_word)
+print(" ".join(words))
+
+def lab32():
+    i=False
+    S=[]
+while i ==False
+inp=input()
+if inp == 'stop':
+i=True
+else:
+s.append(inp)
+print("".join(s))
+
+def lab3():
+word=input()
+if 'ф' in word
+    print('Ого!Это редкое слово!')
+elif 'ф' in word:
+    print ('Ого!Это редкое слово!')
+else:
+    print('Эхх...Это не очень редкое слово...')
+
+
+    def lab4():
+        from random import randint
+        i=0
+        j=0
+        while i < 3:
+            a=randint(1,100)
+            b=randint(1,100)
+            res=int(input(str(a)+ '+' + str(b) + '='))
+            if res !=a + b:
+print('ответ неверный')
+i += 1
+else:
+print('Правильно!')
+j += 1
+if i == 3:
+    print('игра окончена.Правильных ответов:'j)
+
+
+
+
